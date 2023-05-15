7c41c58220c28ec89ff84b991167b7d77b403223
Brent Vatne <brentvatne@gmail.com>
2023-05-15 16:22:14 -0700

    initial commit with yjs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            'crypto': 'react-native-quick-crypto',
+            'stream': 'stream-browserify',
+            'buffer': '@craftzdog/react-native-buffer',
+          },
+        },
+      ],
+    ]
   };
 };
---
+++ crypto-polyfill.js
@@ -0,0 +1,4 @@
+import crypto from 'react-native-quick-crypto';
+
+window.crypto = crypto;
+global.crypto = window.crypto;
